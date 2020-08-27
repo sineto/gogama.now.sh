b107167680469246b4d0f77746fcd5491e5d6163
Sinésio Neto <sn.sineto@gmail.com>
2020-08-27 06:00:11 -0300

    feat: add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MainHeader, Hero } from './style';
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <Hero>
+        <img src='/images/hero-header.png' />
+      </Hero>
+    </MainHeader>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const MainHeader = styled.header`
+  width: 100%;
+`;
+
+export const Hero = styled.div`
+  padding: 10rem 0;
+  display: flex;
+  justify-content: center;
+  img { 
+    max-width: 1132px;
+    margin: 0 auto;
+  }
+`;
