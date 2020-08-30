54fd56a4c28666dcb0e7640e6c115180664799f5
Sinésio Neto <sn.sineto@gmail.com>
2020-08-29 23:08:01 -0300

    chore: remove strict mode react porp

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import 'antd/dist/antd.css'
-// import 'antd/es/modal/style/index.css'
 import './style/fonts.css';
 import './style/global.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
