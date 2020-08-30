417af18708defec416f88cb84d5b03e56c6d66c7
Sinésio Neto <sn.sineto@gmail.com>
2020-08-30 03:32:28 -0300

    chore: create a Main componenet

---
+++ src/components/Main/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import About from '../../components/About';
+import Topics from '../../components/Topics';
+import Team from '../../components/Team';
+
+const Main = () => {
+  return (
+    <> 
+      <About />
+      <Topics />
+      <Team />
+    </>
+  );
+};
+
+export default Main;
+
